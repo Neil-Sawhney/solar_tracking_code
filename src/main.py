b119ab520685e20329ff6a04f0b63ecaf771ae61
Neil Sawhney <Neil@neil-sawhney.com>
2024-04-30 08:26:05 -0400

    put homing back

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
-# TODO: uncomment this
-# import helpers.gpio as gpio
+import helpers.gpio as gpio
 from modules.actuation import follow_plan
 from modules.planner import get_daily_plan
 
 
 def main():
     while True:
-        # # home the actuator TODO: uncomment this
-        # gpio.expand_actuator(10e3)
+        # home the actuator
+        gpio.expand_actuator(10e3)
 
         daily_plan = get_daily_plan()
         follow_plan(daily_plan)
