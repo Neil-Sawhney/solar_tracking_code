b7d03a6cf96fb4400b88c046b09b103723faa890
Neil Sawhney <Neil@neil-sawhney.com>
2024-04-30 11:03:07 -0400

    put stash in startup script

--- start_panel_process.sh
+++ start_panel_process.sh
@@ -1,6 +1,7 @@
 # Check if main.py is already running
 if ! pgrep -f "python -u ./main.py" > /dev/null
 then
+    git stash
     git pull
     sudo chmod +x ./main.py
     nohup python -u ./main.py > output.log 2>&1 &
