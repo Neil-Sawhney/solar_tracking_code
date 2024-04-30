09f5b7a275f75244e13353dbd4078722565df637
Neil Sawhney <Neil@neil-sawhney.com>
2024-04-29 22:37:21 -0400

    idk

--- src/helpers/gpio.py
+++ src/helpers/gpio.py
@@ -1,5 +1,6 @@
-import RPi.GPIO as GPIO
 import time
+
+import RPi.GPIO as GPIO
 
 import src.helpers.hardware_config as cfg
 
@@ -12,13 +13,14 @@ GPIO.setup(cfg.expand_actuator_pin, GPIO.OUT)
 def expand_actuator(milliseconds):
     GPIO.output(cfg.expand_actuator_pin, bool)
     GPIO.output(cfg.contract_actuator_pin, not bool)
+    time.sleep(milliseconds / 1000)
     GPIO.output(cfg.contract_actuator_pin, not bool)
     GPIO.output(cfg.expand_actuator_pin, not bool)
+
 
 def contract_actuator(milliseconds):
     GPIO.output(cfg.contract_actuator_pin, bool)
     GPIO.output(cfg.expand_actuator_pin, not bool)
-    time.sleep(milliseconds)
+    time.sleep(milliseconds / 1000)
     GPIO.output(cfg.contract_actuator_pin, not bool)
     GPIO.output(cfg.expand_actuator_pin, not bool)
-
