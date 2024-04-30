a393c4163ecf95e143d35d1ebaf7d76344155260
Neil Sawhney <Neil@neil-sawhney.com>
2024-04-30 11:00:35 -0400

    fix pull

--- start_panel_process.sh
+++ start_panel_process.sh
@@ -1,9 +1,8 @@
-git pull
-sudo chmod +x ./main.py
-
 # Check if main.py is already running
 if ! pgrep -f "python -u ./main.py" > /dev/null
 then
+    git pull
+    sudo chmod +x ./main.py
     nohup python -u ./main.py > output.log 2>&1 &
 fi
 
