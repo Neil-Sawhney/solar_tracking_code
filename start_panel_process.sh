fd0eaf31d1043046c8522bce5f151bb57bc9c238
Neil Sawhney <Neil@neil-sawhney.com>
2024-04-30 10:55:16 -0400

    avoid duplicate processes

--- start_panel_process.sh
+++ start_panel_process.sh
@@ -1,3 +1,10 @@
+git pull
 sudo chmod +x ./main.py
-nohup python -u ./main.py > output.log 2>&1 &
+
+# Check if main.py is already running
+if ! pgrep -f "python -u ./main.py" > /dev/null
+then
+    nohup python -u ./main.py > output.log 2>&1 &
+fi
+
 tail -f output.log
