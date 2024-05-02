48ce9d189e20535155d0acb177a452f976c850b6
Neil Sawhney <neil@neil-sawhney.com>
2024-05-02 17:30:22 -0400

    test consistency added

--- debug_tests.py
+++ debug_tests.py
@@ -3,4 +3,4 @@ from tests import test_fatigue, test_move_actuator
 if __name__ == "__main__":
     # test_move_actuator.test_contract_actuator()
     # test_move_actuator.test_expand_actuator()
-    test_fatigue.test_fatigue()
+    # test_fatigue.test_fatigue()
